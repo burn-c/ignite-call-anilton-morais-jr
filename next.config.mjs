cf006e312bc77f666f780a325d4267f2e9886764
AniltonFMoraisJr <anilton.morais@bovexo.com>
2024-06-12 14:09:40 -0300

    chore: Project setup

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  // eslint-disable-next-line prettier/prettier
+  pageExtensions: ["page.tsx", "api.ts", "api.tsx"],
 };
 
 export default nextConfig;
