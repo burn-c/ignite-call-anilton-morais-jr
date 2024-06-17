c2e6a00d1aee6fbc775c4833e2ae2f08e11529d0
AniltonFMoraisJr <anilton.morais@bovexo.com>
2024-06-17 14:06:21 -0300

    feat: Installing new components feat: Implementing new component ClaimUsernameForm feat: Editign page Home

---
+++ src/pages/home/components/ClaimUsernameForm/index.tsx
@@ -0,0 +1,64 @@
+import React from 'react'
+import { FormAnnotation, FormClaimUsername } from './styles'
+import { Button, Text, TextInput } from '@ignite-ui/react'
+import { useForm } from 'react-hook-form'
+import { z } from 'zod'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { ArrowRight } from 'phosphor-react'
+
+// import { Container } from './styles';
+
+const claimUsernameFormSchema = z.object({
+  username: z
+    .string()
+    .min(3, { message: 'O usuário precisa ter pelo menos 3 letras' })
+    .regex(/^[a-z\\-]+)$/i, {
+      message: 'O usuário precisa ter apenas letras e hífens.',
+    })
+    .transform((username) => username.toLocaleLowerCase()),
+})
+
+type ClaimUsernameFormData = z.infer<typeof claimUsernameFormSchema>
+
+const ClaimUsernameForm: React.FC = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<ClaimUsernameFormData>({
+    resolver: zodResolver(claimUsernameFormSchema),
+  })
+
+  async function handleClaimUsername(data: ClaimUsernameFormData) {
+    console.log(data)
+  }
+
+  return (
+    <>
+      <FormClaimUsername as="form" onSubmit={handleSubmit(handleClaimUsername)}>
+        <TextInput
+          size="sm"
+          prefix="ignite.com/"
+          placeholder="seu-usuario"
+          crossOrigin={undefined}
+          onPointerEnterCapture={undefined}
+          onPointerLeaveCapture={undefined}
+          {...register('username')}
+        />
+        <Button size={'sm'} type="submit">
+          Reservar
+          <ArrowRight />
+        </Button>
+      </FormClaimUsername>
+      <FormAnnotation>
+        <Text size={'sm'}>
+          {errors.username
+            ? errors.username.message
+            : 'Digite o nome do usuário desejado'}
+        </Text>
+      </FormAnnotation>
+    </>
+  )
+}
+
+export default ClaimUsernameForm
