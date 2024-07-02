5192900de034b4acf0e2550ff8cb4710d52adb4e
AniltonFMoraisJr <anilton.morais@bovexo.com>
2024-07-02 13:50:47 -0300

    feat: Implementing Confirm Step component

--- src/pages/schedule/[username]/ScheduleForm/ConfirmStep/index.tsx
+++ src/pages/schedule/[username]/ScheduleForm/ConfirmStep/index.tsx
@@ -1,9 +1,90 @@
 import React from 'react'
+import { ConfirmForm, FormActions, FormError, FormHeader } from './styles'
+import { Button, Text, TextArea, TextInput } from '@ignite-ui/react'
+import { CalendarBlank, Clock } from 'phosphor-react'
+import { z } from 'zod'
+import { useForm } from 'react-hook-form'
+import { zodResolver } from '@hookform/resolvers/zod'
 
 // import { Container } from './styles';
 
+const confirmFormSchema = z.object({
+  name: z.string().min(3, {
+    message: 'O nome precisa de no mínimo 3 caracteres.',
+  }),
+  email: z.string().email('Digite um e-mail válido'),
+  observations: z.string().nullable(),
+})
+
+type ConfirmFormData = z.infer<typeof confirmFormSchema>
+
 const ConfirmStep: React.FC = () => {
-  return <div />
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isSubmitting },
+  } = useForm<ConfirmFormData>({
+    resolver: zodResolver(confirmFormSchema),
+  })
+
+  function handleConfirmScheduling(data: ConfirmFormData) {
+    console.log(data)
+  }
+
+  return (
+    <ConfirmForm as="form" onSubmit={handleSubmit(handleConfirmScheduling)}>
+      <FormHeader>
+        <Text>
+          <CalendarBlank />
+          02 de Julho de 2024
+        </Text>
+        <Text>
+          <Clock />
+          18:00h
+        </Text>
+      </FormHeader>
+      <label>
+        <Text size={'sm'}>Nome Completo</Text>
+        <TextInput
+          placeholder="Seu nome"
+          crossOrigin={undefined}
+          onPointerEnterCapture={undefined}
+          onPointerLeaveCapture={undefined}
+          {...register('name')}
+        />
+        {errors.name && (
+          <FormError size={'sm'}>{errors.name.message}</FormError>
+        )}
+      </label>
+      <label>
+        <Text size={'sm'}>Endereço de e-mail</Text>
+        <TextInput
+          type="email"
+          placeholder="Seu nome"
+          crossOrigin={undefined}
+          onPointerEnterCapture={undefined}
+          onPointerLeaveCapture={undefined}
+          {...register('email')}
+        />
+        {errors.email && (
+          <FormError size={'sm'}>{errors.email.message}</FormError>
+        )}
+      </label>
+      <label>
+        <Text size={'sm'}>Observações</Text>
+        <TextArea {...register('observations')} />
+      </label>
+
+      <FormActions>
+        <Button type="button" variant={'tertiary'}>
+          Cancelar
+        </Button>
+        <Button type="submit" disabled={isSubmitting}>
+          Confirmar
+        </Button>
+      </FormActions>
+    </ConfirmForm>
+  )
 }
 
 export default ConfirmStep
--- src/pages/schedule/[username]/ScheduleForm/index.tsx
+++ src/pages/schedule/[username]/ScheduleForm/index.tsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import CalendarStep from './CalendarStep'
+import ConfirmStep from './ConfirmStep'
 
 // import { Container } from './styles';
 
 const ScheduleForm: React.FC = () => {
-  return <CalendarStep />
+  return <ConfirmStep />
 }
 
 export default ScheduleForm
